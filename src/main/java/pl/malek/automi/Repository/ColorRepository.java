6483cf2b81437170717d7a6504356593daefaf49
Michał Małek <michal.malek98@wp.pl>
2021-07-27 18:17:49 +0200

    delete test

--- src/main/java/pl/malek/automi/Repository/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.malek.automi.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.malek.automi.Entities.ColorEntity;
-
-@Repository
-public interface ColorRepository extends JpaRepository<ColorEntity, Long> {
-}
--- src/main/java/pl/malek/automi/Repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.malek.automi.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.malek.automi.Entities.ModelEntity;
-
-@Repository
-public interface ModelRepository extends JpaRepository<ModelEntity, Long> {
-}
