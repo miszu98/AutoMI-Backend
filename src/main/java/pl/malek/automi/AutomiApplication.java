1085172160b80940a641cdacc3a262f257bd73cd
Michał Małek <men0709@wp.pl>
2021-07-27 16:24:15 +0200

    init backend

---
+++ src/main/java/pl/malek/automi/AutomiApplication.java
@@ -0,0 +1,13 @@
+package pl.malek.automi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/malek/automi/AutomiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.malek.automi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
