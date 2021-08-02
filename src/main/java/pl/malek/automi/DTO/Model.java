9bf421429ab662f7b67ce2e8b868e34f7d5f552f
Michał Małek <michal.malek98@wp.pl>
2021-08-02 20:33:05 +0200

    created validation for car model

--- src/main/java/pl/malek/automi/DTO/Model.java
+++ src/main/java/pl/malek/automi/DTO/Model.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Data
@@ -12,6 +15,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Model {
     private Long id;
+
+    @Size(min = 3, max = 24, message = "length (5-24)")
+    @NotBlank(message = "Model field may be not blank")
     private String model;
+
+    @NotNull(message = "Mark id is needed to define car model")
     private Long markId;
 }
