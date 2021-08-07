1f3dbdb9f56a099d6955952c2295289eba7dd190
Michał Małek <michal.malek98@wp.pl>
2021-08-07 13:55:43 +0200

    added to repositories method to find by name entity

--- src/main/java/pl/malek/automi/Repository/CarRepository.java
+++ src/main/java/pl/malek/automi/Repository/CarRepository.java
@@ -3,8 +3,11 @@ package pl.malek.automi.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.malek.automi.Entities.CarEntity;
+import pl.malek.automi.Entities.ModelEntity;
+
+import java.util.Optional;
 
 @Repository
 public interface CarRepository extends JpaRepository<CarEntity, Long> {
-    boolean existsById(long id);
+    Optional<CarEntity> findCarEntityByModel(ModelEntity modelEntity);
 }
--- src/main/java/pl/malek/automi/Repository/ColorRepository.java
+++ src/main/java/pl/malek/automi/Repository/ColorRepository.java
@@ -3,7 +3,12 @@ package pl.malek.automi.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.malek.automi.Entities.ColorEntity;
+import pl.malek.automi.Entities.FuelTypeEntity;
+
+import java.util.Optional;
 
 @Repository
 public interface ColorRepository extends JpaRepository<ColorEntity, Long> {
+    Optional<ColorEntity> findColorEntityByColorName(String colorName);
+
 }
