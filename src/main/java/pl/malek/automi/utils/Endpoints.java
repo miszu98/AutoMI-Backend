ffcf1107ffb1dc0a88641a13a23d2022c655db35
Michał Małek <michal.malek98@wp.pl>
2021-08-27 20:39:19 +0200

    created Endpoints class which represent all endpoints of api

---
+++ src/main/java/pl/malek/automi/utils/Endpoints.java
@@ -0,0 +1,13 @@
+package pl.malek.automi.utils;
+
+public class Endpoints {
+    public static final String USERS = "/users/";
+    public static final String ROLES = "/roles/";
+    public static final String MARKS = "/marks/";
+    public static final String MODELS = "/models/";
+    public static final String GEARBOX = "/gearbox/";
+    public static final String FUEL_TYPES = "/fuel-types/";
+    public static final String DRIVING_GEARS = "/driving-gears/";
+    public static final String COLORS = "/colors/";
+    public static final String CAR_OFFERS = "/car-offers/";
+}
