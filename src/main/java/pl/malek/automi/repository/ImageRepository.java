c245e3597d334046fbd95a94611504988f0348ae
Michał Małek <michal.malek98@wp.pl>
2021-09-24 18:42:37 +0200

    created method to delete images by offer id

--- src/main/java/pl/malek/automi/repository/ImageRepository.java
+++ src/main/java/pl/malek/automi/repository/ImageRepository.java
@@ -1,9 +1,16 @@
 package pl.malek.automi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.malek.automi.entity.ImageEntity;
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
+    @Modifying
+    @Query("delete from ImageEntity img where img.carOfferEntity.id = :offerId")
+    void deleteImagesByOfferId(@Param("offerId") Long offerId);
 }
