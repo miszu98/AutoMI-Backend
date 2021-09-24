368f2b35591c40fd98d5ec3af1e7206e33328150
Michał Małek <michal.malek98@wp.pl>
2021-09-24 18:48:15 +0200

    created method deleteImagesByOfferId added implementation of upload method now is possible to upload image by file and by url

--- src/main/java/pl/malek/automi/service/Impl/ImageServiceImpl.java
+++ src/main/java/pl/malek/automi/service/Impl/ImageServiceImpl.java
@@ -6,27 +6,75 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import pl.malek.automi.entity.ImageEntity;
+import pl.malek.automi.exception.CarOfferNotFoundException;
+import pl.malek.automi.repository.CarOfferRepository;
+import pl.malek.automi.repository.ImageRepository;
 import pl.malek.automi.service.ImageService;
+
+import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 @Service
 public class ImageServiceImpl implements ImageService {
 
     private Cloudinary cloudinary;
+    private ImageRepository imageRepository;
+    private CarOfferRepository carOfferRepository;
 
-    public ImageServiceImpl(@Value("${CLOUDINARY_URL}") String config) {
+    public ImageServiceImpl(@Value("${CLOUDINARY_URL}") String config, ImageRepository imageRepository, CarOfferRepository carOfferRepository) {
         cloudinary = new Cloudinary(config);
+        this.imageRepository = imageRepository;
+        this.carOfferRepository = carOfferRepository;
     }
 
     @Override
-    public String upload(MultipartFile file) throws IOException {
-        var map = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
-                "eager", Arrays.asList(
-                        new EagerTransformation().width(400).height(300).crop("pad"),
-                        new EagerTransformation().width(260).height(200).crop("crop").gravity("north")
-                )));
-        return (String) map.get("url");
+    public List<String> upload(MultipartFile[] files, Long offerId) throws IOException, CarOfferNotFoundException {
+        var links = new ArrayList<String>();
+
+        var foundedOffer = carOfferRepository.findById(offerId).orElseThrow(
+                () -> new CarOfferNotFoundException(String.format("Not found Car offer with id: %d", offerId))
+        );
+
+        for (MultipartFile image : files) {
+            var map = cloudinary.uploader().upload(image.getBytes(), ObjectUtils.asMap(
+                    "eager", Arrays.asList(
+                            new EagerTransformation().width(400).height(300).crop("pad"),
+                            new EagerTransformation().width(260).height(200).crop("crop").gravity("north")
+                    )));
+
+            var link = (String) map.get("url");
+            links.add(link);
+
+            var imgEntity = ImageEntity.builder().url(link).carOfferEntity(foundedOffer).build();
+
+            imageRepository.save(imgEntity);
+        }
+
+        return links;
+    }
+
+    @Override
+    public List<String> upload(List<String> files, Long offerId) throws CarOfferNotFoundException {
+        var foundedOffer = carOfferRepository.findById(offerId).orElseThrow(
+                () -> new CarOfferNotFoundException(String.format("Not found Car offer with id: %d", offerId))
+        );
+
+        for (String link : files) {
+            var imgEntity = ImageEntity.builder().url(link).carOfferEntity(foundedOffer).build();
+            imageRepository.save(imgEntity);
+        }
+
+        return files;
+    }
+
+    @Override
+    @Transactional
+    public void deleteImagesByOfferId(Long id) {
+        imageRepository.deleteImagesByOfferId(id);
     }
 }
