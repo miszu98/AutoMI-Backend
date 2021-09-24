37e130d4c2cb027284bad028da477633c84942f0
Michał Małek <michal.malek98@wp.pl>
2021-09-24 18:29:40 +0200

    added id field and modified constructor

--- src/main/java/pl/malek/automi/dto/JwtResponse.java
+++ src/main/java/pl/malek/automi/dto/JwtResponse.java
@@ -10,14 +10,16 @@ import java.io.Serializable;
 public class JwtResponse implements Serializable {
     private static final long serialVersionUID = 1;
 
+    private Long id;
     private String email;
     private String token;
     private String role;
 
-    public JwtResponse(String token, String email, String role) {
+    public JwtResponse(String token, String email, String role, Long id) {
         this.email = email;
         this.token = token;
         this.role = role;
+        this.id = id;
     }
 
 }
