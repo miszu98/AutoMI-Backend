5c492976b30a30e5710c3596321dcd4a20ffe3ca
Michał Małek <michal.malek98@wp.pl>
2021-09-24 18:07:01 +0200

    created endpoint which allow to delete all images from specific offer and created second implementation of upload images method now exists endpoint to upload images by strings/links/urls

--- src/main/java/pl/malek/automi/controller/ImageUploaderController.java
+++ src/main/java/pl/malek/automi/controller/ImageUploaderController.java
@@ -3,14 +3,14 @@ package pl.malek.automi.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import pl.malek.automi.dto.Urls;
+import pl.malek.automi.exception.CarOfferNotFoundException;
 import pl.malek.automi.service.ImageService;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -20,8 +20,18 @@ public class ImageUploaderController {
     private final ImageService imageService;
 
     @PostMapping("/")
-    public ResponseEntity<String> upload(@RequestBody MultipartFile file) throws IOException {
-        return ResponseEntity.status(HttpStatus.OK).body(imageService.upload(file));
+    public ResponseEntity<List<String>> upload(Long offerId, @RequestBody MultipartFile... files) throws IOException, CarOfferNotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(imageService.upload(files, offerId));
     }
 
+    @PostMapping("/s/")
+    public ResponseEntity<List<String>> upload(@RequestBody Urls urls) throws IOException, CarOfferNotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(imageService.upload(urls.getLinks(), urls.getOfferId()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> upload(@PathVariable Long id){
+        imageService.deleteImagesByOfferId(id);
+        return ResponseEntity.status(HttpStatus.OK).body("delete");
+    }
 }
